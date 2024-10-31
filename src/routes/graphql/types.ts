07644421524c10d9be2747029a25e6450d15a30e
Volha Yakauleva <yvvolha@gmail.com>
2024-10-31 23:12:45 +0200

    feat: add some more types

--- src/routes/graphql/types.ts
+++ src/routes/graphql/types.ts
@@ -1,11 +1,29 @@
 import {GraphQLFloat, GraphQLObjectType, GraphQLString } from "graphql";
 import { UUIDType } from "./types/uuid.js";
+import { PrismaClient } from "@prisma/client";
+
+type Context = {
+	prisma: PrismaClient,
+}
 
 export const userType = new GraphQLObjectType({
-    name: "User",
-    fields: {
-        id: {type: UUIDType},
-        name: {type: GraphQLString},
-        balance: {type: GraphQLFloat},
+  name: "User",
+	fields: {
+			id: {type: UUIDType},
+			name: {type: GraphQLString},
+			balance: {type: GraphQLFloat},
+	},
+})
+
+
+export const queryType = new GraphQLObjectType({
+  name: "Query",
+  fields: {
+    users: {
+      type: userType,
+      resolve: async (_obj, _args, { prisma }: Context) => {
+        return prisma.user.findMany();
+      },
     },
-  })
+  },
+})
