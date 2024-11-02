b0ba5ee3476edfe770e6291dd99586ad1a55e039
Volha Yakauleva <yvvolha@gmail.com>
2024-11-02 13:22:29 +0200

    feat: add getting a single memberType by their id

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,7 +1,7 @@
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
 import { createGqlResponseSchema, gqlResponseSchema } from './schemas.js';
 import { graphql } from 'graphql';
-import { myGraphQlSchema } from './types.js';
+import { myGraphQlSchema } from './graphql-types.js';
 import { Context } from './types/context.js';
 
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
