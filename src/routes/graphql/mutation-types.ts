0be1c2263e45e746c2872104477d56abe2ad2f2b
Volha Yakauleva <yvvolha@gmail.com>
2024-11-02 20:23:20 +0200

    feat: create creating a user

--- src/routes/graphql/mutation-types.ts
+++ src/routes/graphql/mutation-types.ts
@@ -1,6 +1,37 @@
-import { GraphQLObjectType } from "graphql";
+import { GraphQLFloat, GraphQLInputObjectType, GraphQLNonNull, GraphQLObjectType, GraphQLString } from "graphql";
+
+import { userType } from "./entities-types.js";
+import { Context } from "./types/context.js";
+import { ICreateUserInput } from "./types/mutation-fields.js";
+
+interface DtoType<T> {
+  dto: T;
+};
+
+//users
+const createUserInput = new GraphQLInputObjectType({
+  name: 'CreateUserInput',
+  fields: {
+    name: { type: GraphQLString },
+    balance: { type: GraphQLFloat },
+  },
+});
+
+
 
 export const mutationType = new GraphQLObjectType({
-    name: "Mutation",
-    fields: () => ({}),
+  name: "Mutation",
+  fields: () => ({
+    // users
+    createUser: {
+      type: new GraphQLNonNull(userType),
+      args: { dto: { type: createUserInput }},
+      resolve: (_obj, { dto } : DtoType<ICreateUserInput>, { prisma }: Context) => {
+        console.log('here is DTO:::', dto)
+          return prisma.user.create({
+            data: dto,
+          });
+      },
+    }
+  }),
 })
